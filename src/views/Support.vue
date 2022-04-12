e3f3b490cc0e1d987a086611800fc755e6b6b828
hbao-newbie <baob1906432@student.ctu.edu.vn>
2022-04-12 23:24:48 +0700

    Support.vue

--- src/views/Support.vue
+++ src/views/Support.vue
@@ -1,13 +1,47 @@
-<script>
-
-</script>
-
 <template>
-    <h1>Support</h1>
+    <div class="d-flex flex-column container box">
+        <div class="p-2">
+            <h3 class="display-5 text-center">CHÍNH SÁCH BẢO MẬT THÔNG TIN</h3>
+            <div>
+                <h4>1. THÔNG TIN CÁC NHÂN</h4>
+                <p>Shoes store chỉ yêu cầu các thông tin cá nhân của bạn như</p>
+                <ul>
+                    <li>Họ và tên</li>
+                    <li>Ngày, tháng, năm sinh</li>
+                    <li>Địa chỉ email</li>
+                    <li>Địa chỉ giao hàng</li>
+                </ul>
+            </div>
+            <div>
+                <h4>2. CÁCH THỨC SỬ DỤNG THÔNG TIN</h4>
+                <p>Thông thường, chúng tôi sử dụng các thông tin bạn cung cấp chỉ để liên hệ, hồi đáp những câu hỏi hay xác nhận thông tin đơn hàng theo tên, địa chỉ và số điện thoại mà bạn đã cung cấp. Chúng tôi cam kết thông tin cá nhân của bạn sẽ không bị chia sẻ với bất kỳ một bên thứ ba nào khi chưa có sự đồng ý. Chúng tôi chỉ có thể được phép chia sẻ thông tin với các đối tác giao nhận hoặc với các đối tác trong các chương trình, dự án liên kết khác. Dữ liệu khách hàng của Shoes store có thể được chuyển nhượng cho người kế thừa hay người được chỉ định theo thỏa thuận để quản lý khi công ty có sự mua bán, sát nhập hay tuyên bố phá sản.</p>
+            </div>
+        </div>
+        <div class="p-2">
+            <h3 class="display-5 text-center">ĐIỀU KHOẢN SỬ DỤNG</h3>
+            <div>
+                <h4>1. THƯƠNG HIỆU VÀ BẢN QUYỀN</h4>
+                <p>Mọi quyền sở hữu trí tuệ (đã đăng ký hoặc chưa đăng ký), nội dung thông tin và tất cả các thiết kế, văn bản, đồ họa, hình ảnh, video, âm nhạc, mã nguồn,… trong phạm vi website bạn đang xem đều thuộc sở hữu tài sản của Shoes store. Toàn bộ nội dung của trang web được bảo vệ bởi luật bản quyền của Việt Nam và các công ước quốc tế.</p>
+            </div>
+            <div>
+                <h4>2. QUY ĐỊNH VỀ TRUY CẬP VÀ SỬ DỤNG WEBSITE</h4>
+                <p>Khi truy cập vào website của chúng tôi, bạn cần đảm bảo việc bạn có đủ hành vi dân sự để thực hiện các giao dịch mua hàng theo quy định hiện hành của pháp luật Việt Nam. Hãy đảm bảo rằng bạn đã đủ 16 tuổi hoặc truy cập dưới sự giám sát của các thành viên trong gia đình hay người giám hộ hợp pháp.
+                Trong suốt quá trình đăng ký, sử dụng website,… bạn có toàn quyền quyết định mình sẽ được nhận thư quảng cáo, tin tức về các chương trình khuyến mãi thông qua Email mà bạn đã đăng ký hay không. Nếu không muốn tiếp tục nhận Email từ Shoes store, bạn cũng có toàn quyền từ chối bằng cách nhấp vào đường dẫn ở dưới cùng trong thư điện tử được gửi đến bất kỳ.
+                Shoes store không mong muốn bạn sử dụng bất kỳ chương trình, công cụ hay hình thức nào khác để can thiệp vào nội dung, hệ thống hay làm thay đổi cấu trúc dữ liệu của website chúng tôi. Các hình thức phát tán, truyền bá, cổ vũ không được phép hay can thiệp, thay đổi, xóa bỏ nội dung, cấu trúc dữ liệu hệ thống của chúng tôi một cách bất hợp pháp đều sẽ có hình thức xử lý từ Shoes store. Mọi cá nhân hoặc tổ chức nếu vi phạm sẽ chịu truy tố trước pháp luật và phải bồi thường thiệt hại đã gây ra.</p>
+            </div>
+        </div>
+        <div class="draw"></div>
+    </div>
 </template>
 
 <style>
-    h1{
-        text-align: center;
-    }
+.box{
+    margin-top: 10px;
+}
+
+.draw{
+    width: 100%;
+    height: 1px;
+    background-color: gray;
+}
 </style>
