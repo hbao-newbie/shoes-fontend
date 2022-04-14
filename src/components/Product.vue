35c28e674f0a808f38680ab08f1a0bf5186f4350
hbao-newbie <baob1906432@student.ctu.edu.vn>
2022-04-15 00:35:34 +0700

    Product list

---
+++ src/components/Product.vue
@@ -0,0 +1,29 @@
+<template>
+    <div
+        v-for="product in products" :key="product.id"
+        class="p-2" style="width: 19rem;"
+    >
+        <div class="card">
+            <img :src="`/products/${product.imgProduct}.jpg`" style="width: 18rem;" class="card-img-top">
+            <div class="card-body" style="height: 250px;">
+                <h5 class="card-title">{{ product.nameProduct }}</h5>
+                <p class="card-text">{{ product.description }}</p>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Mã sản phẩm {{ product.codeProduct }}</li>
+                <li class="list-group-item">Giá {{ product.costProduct }}.000 VND</li>
+            </ul>
+            <div class="card-body">
+                <button class="btn btn-primary">Thêm vào giỏ hàng</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        products: { type: Array, default: [] },
+    },
+};
+</script>
--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,20 +1,84 @@
 <template>
     <div class="container">
-        <SearchProductVue class="input-search"/>
+        <SearchProductVue class="input-search" />
+    </div>
+    <div class="d-flex flex-wrap product-list">
+        <ProductVue
+            :products="products"
+        />
     </div>
 </template>
 
 <script>
-import SearchProductVue from "../components/SearchProduct.vue"
+import productService from "../services/product.service";
+import SearchProductVue from "../components/SearchProduct.vue";
+import ProductVue from "../components/Product.vue";
 export default {
     components: {
         SearchProductVue,
-    }
+        ProductVue,
+    },
+    data() {
+        return {
+            products: [],
+            // activeIndex: -1,
+            // searchText: "",
+        };
+    },
+    // watch: {
+    //     searchText() {
+    //         this.activeIndex = -1;
+    //     },
+    // },
+    computed: {
+        // Chuyển các đối tượng thành chuỗi để tìm kiếm
+        // productStrings() {
+        //     return this.products.map((product) => {
+        //         const { nameProduct, codeProduct } = product;
+        //         return [ nameProduct, codeProduct ].join("");
+        //     });
+        // },
+        // Trả về các đối tượng products cần tìm kiếm
+        // filteredProduct() {
+        //     if (!this.searchText) return this.products;
+        //     return this.products.filter(
+        //         (product, index) => this.productStrings[index].includes(this.searchText)
+        //     );
+        // },
+        // activeProduct() {
+        //     if (this.activeIndex < 0) return null;
+        //     return this.filteredProduct[this.activeIndex];
+        // },
+        // productCount(){
+        //     return this.filteredProduct.length;
+        // }
+    },
+    methods: {
+        async retriveProduct(){
+            try {
+                this.products = await productService.getAll();
+            } catch(err) {
+                console.log(err);
+            }
+        },
+        refreshList() {
+            this.retriveProduct();
+            // this.activeIndex = -1;
+        }
+    },
+    mounted() {
+        this.refreshList();
+    },
+
 }
 </script>
 
-<style>
+<style scoped>
 .input-search {
     margin: 10px 0 10px;
+}
+
+.product-list {
+    margin: 0 9% 0;
 }
 </style>
